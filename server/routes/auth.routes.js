e51918ac9cd220054e46cfe0009ded0e0f74e7cf
Dmitry <svetlana260574@protonmail.com>
2021-05-15 02:50:50 -0500

    working on authorisation

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -25,8 +25,7 @@ router.post("/register", async (req, res) => {
       console.log("Ошибка кандидат найден");
       return res.status(400).json({ message: "User already exist" });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
       email,
